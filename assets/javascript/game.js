2cef263b6b402ab79ee35fd36d68f8fec00ba413
carlosalfonzo33 <carlos.alfonzo@gmail.com>
2017-05-09 17:42:19 -0700

    1st commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+//Array of letters
+
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+
+//Computer randomly selects the word 
+var compChoice = Math.floor(Math.random() * letters.length);
+console.log(compChoice);
