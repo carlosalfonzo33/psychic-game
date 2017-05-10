3546cd4396d0398cd17fa70a01b15cd91bfc26fd
carlosalfonzo33 <carlos.alfonzo@gmail.com>
2017-05-10 16:11:57 -0700

    More changes on both css and html files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
-//Letter choices available
+//my array of the alphabet
 var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-//Setting all to zero
+//my var settings 
 var wins = 0;
 var losses = 0;
 var guesses = 9;
@@ -11,13 +11,12 @@ var letterToGuess = null;
 
 
 
-//Lets the computer select a random letter from the available choices
+//computer chooses a random letter from array.
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
 //Allows the user 9 guesses
-// guesses = guesses || 9
 var updateGuessesLeft = function() {
-  // Here we are grabbing the HTML element and setting it equal to the guessesLeft. (i.e. guessesLeft will get displayed in HTML)
+  // HTML element and setting it equal to the guessesLeft. (i.e. guessesLeft will get displayed in HTML)
   document.querySelector('#guessLeft').innerHTML = "Guesses left: " + guessesLeft;
 };
 
