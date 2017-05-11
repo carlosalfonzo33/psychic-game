32b8241ffa60d44b754d9e5cd2bd1fcecb467a31
carlosalfonzo33 <carlos.alfonzo@gmail.com>
2017-05-10 23:57:02 -0700

    final  adjustments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,5 @@
-//my array of the alphabet
 var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-//my var settings 
 var wins = 0;
 var losses = 0;
 var guesses = 10;
@@ -9,25 +7,21 @@ var guessesLeft = 10;
 var guessedLetters = [];
 var letterToGuess = null;
 
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
-
-//computer chooses a random letter from array.
-var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-//Allows the user 9 guesses
-var updateGuessesLeft = function() {
-  // HTML element and setting it equal to the guessesLeft. (i.e. guessesLeft will get displayed in HTML)
+        var updateGuessesLeft = function() {
   document.querySelector('#guessLeft').innerHTML = "Guesses left: " + guessesLeft;
 };
 
 var updateLetterToGuess = function() {
   this.letterToGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
 };
+
 var updateGuessesSoFar = function() {
-  // Here we take the guesses the user has tried -- then display it as letters separated by commas. 
   document.querySelector('#let').innerHTML = "Your Guesses so far: " + guessedLetters.join(', ');
 };
-// Function will be called when we reset everything
+
+//Resets the game 
 var reset = function() {
   totalGuesses = 10;
   guessesLeft = 10;
@@ -42,7 +36,6 @@ updateLetterToGuess();
 updateGuessesLeft();
 
 
-//When key is released it becomes the users guess
 document.onkeyup = function(event) {
     guessesLeft--;
   var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
@@ -59,11 +52,9 @@ document.onkeyup = function(event) {
                 reset();
             }
         }else if(guessesLeft == 0){
-            // Then we will loss and we'll update the html to display the loss 
             losses++;
             document.querySelector('#losses').innerHTML = "Losses: " + losses;
             alert("Sorry, YOU LOSE!  try again?");
-            // Then we'll call the reset. 
             reset();
         }
 };
